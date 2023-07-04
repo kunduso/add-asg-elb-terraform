306d299c336356d55646a764b1a44aed4267f944
Sourav Kundu <skundu.dev@gmail.com>
2023-07-03 23:44:20 -0500

    separate out both rules

--- securitygroup.tf
+++ securitygroup.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "ec2_security_group" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.asg_lb_security_group.id]
+    security_groups = [aws_security_group.aws_security_group.asg_lb_security_group_egress, aws_security_group.asg_lb_security_group_ingress]
   }
 
   egress {
@@ -22,9 +22,9 @@ resource "aws_security_group" "ec2_security_group" {
   }
 }
 
-resource "aws_security_group" "asg_lb_security_group" {
-  name = "ASG-LB-SG"
-  description = "Allow inbound and outbound traffic to load balancer from the internet."
+resource "aws_security_group" "asg_lb_security_group_ingress" {
+  name = "ASG-LB-SG-IN"
+  description = "Allow inbound traffic to load balancer from the internet."
   ingress {
     from_port   = 80
     to_port     = 80
@@ -32,6 +32,15 @@ resource "aws_security_group" "asg_lb_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  vpc_id = aws_vpc.this.id
+  tags = {
+    "Name" = "app-1-asg-lb-sg"
+  }
+}
+
+resource "aws_security_group" "asg_lb_security_group_egress" {
+  name = "ASG-LB-SG-EG"
+  description = "Allow outbound traffic to load balancer from the internet."
   egress {
     from_port   = 0
     to_port     = 0
