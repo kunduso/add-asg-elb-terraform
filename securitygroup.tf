ddc8781fd98d6817eef5bd3f7d686a08692794d8
Sourav Kundu <skundu.dev@gmail.com>
2023-07-03 23:50:54 -0500

    updated security groups

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,12 +1,12 @@
 
-resource "aws_security_group" "ec2_security_group" {
-  name = "Instance-SG"
-  description = "Allow inbound and outbound traffic to EC2 instances from load balancer security group"
+resource "aws_default_security_group" "default" {
+  description = "Ensure AWS Default Security Group restricts all traffic"
+  vpc_id = aws_vpc.this.id
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.aws_security_group.asg_lb_security_group_egress, aws_security_group.asg_lb_security_group_ingress]
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
   }
 
   egress {
@@ -15,39 +15,44 @@ resource "aws_security_group" "ec2_security_group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
+resource "aws_security_group" "ec2_security_group" {
+  name        = "Instance-SG"
+  description = "Allow inbound and outbound traffic to EC2 instances from load balancer security group"
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.aws_security_group.asg_lb_security_group]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   vpc_id = aws_vpc.this.id
   tags = {
     "Name" = "app-1-instance-sg"
   }
 }
 
-resource "aws_security_group" "asg_lb_security_group_ingress" {
-  name = "ASG-LB-SG-IN"
-  description = "Allow inbound traffic to load balancer from the internet."
+resource "aws_security_group" "asg_lb_security_group" {
+  description = "Allow inbound and outbound traffic to load balancer from the internet."
+  name        = "ASG-LB-SG-IN"
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  vpc_id = aws_vpc.this.id
-  tags = {
-    "Name" = "app-1-asg-lb-sg"
-  }
-}
-
-resource "aws_security_group" "asg_lb_security_group_egress" {
-  name = "ASG-LB-SG-EG"
-  description = "Allow outbound traffic to load balancer from the internet."
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   vpc_id = aws_vpc.this.id
   tags = {
     "Name" = "app-1-asg-lb-sg"
