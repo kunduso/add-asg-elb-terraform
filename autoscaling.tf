0b2be79d8646b50b744af64033f7f06efd0e0d76
Sourav Kundu <skundu.dev@gmail.com>
2023-08-22 15:16:00 -0500

    added scale out and scale in policies

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "amazon_ami" {
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20220606.1-x86_64-gp2"]
+    values = var.ami_name
   }
   filter {
     name   = "virtualization-type"
@@ -12,19 +12,21 @@ data "aws_ami" "amazon_ami" {
 }
 resource "aws_launch_template" "application" {
   image_id      = data.aws_ami.amazon_ami.id
-  instance_type = "t2.small"
+  instance_type = var.instance_type
   user_data     = filebase64("./user_data/user_data.tpl")
   network_interfaces {
     security_groups = [aws_security_group.ec2_security_group.id]
   }
-
+#   iam_instance_profile {
+#     name = "ec2_profile"
+#   }
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_group" "application" {
-  name             = "app-one"
+  name             = "app-3"
   min_size         = 3
   max_size         = 6
   desired_capacity = 3
@@ -32,13 +34,71 @@ resource "aws_autoscaling_group" "application" {
     id      = aws_launch_template.application.id
     version = "$Latest"
   }
-  vpc_zone_identifier = aws_subnet.public.*.id
+  vpc_zone_identifier = aws_subnet.private.*.id
 
   health_check_type = "ELB"
-
+  lifecycle {
+    create_before_destroy = true
+  }
   tag {
     key                 = "Name"
     value               = "app-3"
     propagate_at_launch = true
   }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
+}
+
+# Creating the autoscaling policy of the autoscaling group
+resource "aws_autoscaling_policy" "asg_policy_up" {
+  name                   = "asg_policy_up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.application.name
+}
+resource "aws_cloudwatch_metric_alarm" "asg_cpu_alarm_up" {
+  alarm_name          = "asg_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "50"
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.application.name}"
+  }
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.asg_policy_up.arn]
+
+}
+
+resource "aws_autoscaling_policy" "asg_policy_down" {
+  name                   = "asg_policy_down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.application.name
+}
+resource "aws_cloudwatch_metric_alarm" "asg_cpu_alarm_down" {
+  alarm_name          = "asg_cpu_alarm_down"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "40"
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.application.name}"
+  }
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.asg_policy_down.arn]
+
 }
