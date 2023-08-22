ab8a577ce9ed640a94cd49952181ed4b056ffe28
Sourav Kundu <skundu.dev@gmail.com>
2023-08-22 15:14:49 -0500

    converted availability zone to data

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 # https://docs.aws.amazon.com/glue/latest/dg/set-up-vpc-dns.html
 resource "aws_vpc" "this" {
-  cidr_block = "10.20.20.0/24"
+  cidr_block = var.vpc_cidr
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_support
   enable_dns_support = true
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_hostnames
@@ -10,11 +10,14 @@ resource "aws_vpc" "this" {
     "Name" = "app-3"
   }
 }
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 resource "aws_subnet" "private" {
   count             = length(var.subnet_cidr_private)
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.subnet_cidr_private[count.index]
-  availability_zone = var.availability_zone[count.index]
+  availability_zone = data.aws_availability_zones.available.names[(count.index) % length(data.aws_availability_zones.available.names)]
   tags = {
     "Name" = "app-3-private-${count.index + 1}"
   }
@@ -23,7 +26,7 @@ resource "aws_subnet" "public" {
   count             = length(var.subnet_cidr_public)
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.subnet_cidr_public[count.index]
-  availability_zone = var.availability_zone[count.index]
+  availability_zone = data.aws_availability_zones.available.names[(count.index) % length(data.aws_availability_zones.available.names)]
   tags = {
     "Name" = "app-3-public-${count.index + 1}"
   }
@@ -72,6 +75,9 @@ resource "aws_nat_gateway" "public" {
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   allocation_id = aws_eip.nat_gateway[count.index].id
   depends_on    = [aws_internet_gateway.this-igw]
+  tags = {
+    "Name" = "app-3-NAT-${count.index + 1}"
+  }
 }
 resource "aws_route" "private-route" {
   count                  = length(var.subnet_cidr_private)
