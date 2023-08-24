50f940b108b4b9c5791714193ba33516218b2807
Sourav Kundu <skundu.dev@gmail.com>
2023-08-24 10:44:21 -0500

    updated life cycle rules and threshold

--- autoscaling.tf
+++ autoscaling.tf
@@ -17,9 +17,9 @@ resource "aws_launch_template" "application" {
   network_interfaces {
     security_groups = [aws_security_group.ec2_security_group.id]
   }
-  #   iam_instance_profile {
-  #     name = "ec2_profile"
-  #   }
+    iam_instance_profile {
+      name = "ec2_profile"
+    }
   lifecycle {
     create_before_destroy = true
   }
@@ -37,24 +37,16 @@ resource "aws_autoscaling_group" "application" {
   vpc_zone_identifier = aws_subnet.private.*.id
 
   health_check_type = "ELB"
-  lifecycle {
-    create_before_destroy = true
-  }
+    lifecycle { 
+        ignore_changes = [desired_capacity, target_group_arns]
+    }
   tag {
     key                 = "Name"
     value               = "app-3"
     propagate_at_launch = true
   }
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["tag"]
-  }
 }
 
-# Creating the autoscaling policy of the autoscaling group
 resource "aws_autoscaling_policy" "asg_policy_up" {
   name                   = "asg_policy_up"
   scaling_adjustment     = 1
@@ -70,13 +62,12 @@ resource "aws_cloudwatch_metric_alarm" "asg_cpu_alarm_up" {
   namespace           = "AWS/EC2"
   period              = "60"
   statistic           = "Average"
-  threshold           = "50"
+  threshold           = "70"
   dimensions = {
     AutoScalingGroupName = "${aws_autoscaling_group.application.name}"
   }
   alarm_description = "This metric monitors ec2 cpu utilization"
   alarm_actions     = [aws_autoscaling_policy.asg_policy_up.arn]
-
 }
 
 resource "aws_autoscaling_policy" "asg_policy_down" {
@@ -94,11 +85,10 @@ resource "aws_cloudwatch_metric_alarm" "asg_cpu_alarm_down" {
   namespace           = "AWS/EC2"
   period              = "60"
   statistic           = "Average"
-  threshold           = "40"
+  threshold           = "30"
   dimensions = {
     AutoScalingGroupName = "${aws_autoscaling_group.application.name}"
   }
   alarm_description = "This metric monitors ec2 cpu utilization"
   alarm_actions     = [aws_autoscaling_policy.asg_policy_down.arn]
-
 }
