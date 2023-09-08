8184ee1339d8d8ca9c6598027c3ca16576539532
Sourav Kundu <skundu.dev@gmail.com>
2023-09-08 16:30:19 -0500

    added instance refresh and remove lifecyle

--- autoscaling.tf
+++ autoscaling.tf
@@ -20,9 +20,6 @@ resource "aws_launch_template" "application" {
   iam_instance_profile {
     name = "ec2_profile"
   }
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "aws_autoscaling_group" "application" {
@@ -36,10 +33,17 @@ resource "aws_autoscaling_group" "application" {
     version = aws_launch_template.application.latest_version
   }
   vpc_zone_identifier = aws_subnet.private.*.id
-
   health_check_type = "ELB"
   lifecycle {
     ignore_changes = [desired_capacity, target_group_arns]
+  }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+      skip_matching = true
+    }
+    triggers = ["launch_template"]
   }
   tag {
     key                 = "Name"
